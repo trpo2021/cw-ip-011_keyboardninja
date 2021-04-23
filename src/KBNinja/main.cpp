3c43e58d33a52ece6c687a3caa5909500cce3283
Ivan <ilitetop@yandex.ru>
2021-04-23 12:44:36 +0700

    In main.cpp added features of the 1st level of difficulty
    In this version, the functionality has been expanded main.cpp,
    added the 1st level of difficulty for 2 modes, prepared layouts for the rest.

--- src/KBNinja/main.cpp
+++ src/KBNinja/main.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <time.h>
 
-void Prepare (int *Mode, int *Difficulty)
+void Prepare (int *Mode, int *Difficulty) // выбираем режим сложности тренировки
 {
     system("PAUSE");
     *Mode = 0, *Difficulty = 0;
-    while (*Mode == 0)
+    while (*Mode == 0) // выбираем что будем печатать
     {
         system("cls");
         printf("\n\n Выбери режим тренировки:\n\t1 - Только буквы\n\t2 - Только цифры\n\t3 - Буквы и цифры\n");
@@ -21,7 +22,7 @@ void Prepare (int *Mode, int *Difficulty)
         }
     }
 
-    while (*Difficulty == 0)
+    while (*Difficulty == 0) // выбираем как будем печатать
     {
         system("cls");
         printf("\nВыдерете сложность печати:\n");
@@ -51,9 +52,9 @@ void Prepare (int *Mode, int *Difficulty)
             *Difficulty = 0;
         }
     }
-}
+}//
 
-bool Exit(bool exit)
+bool Exit(bool exit) // Проверяем хочет ли пользователь закончить игру
 {
     system("cls");
     char key;
@@ -66,23 +67,148 @@ bool Exit(bool exit)
         exit = false;
     }
     return exit;
+} //
+
+bool Comparator(char *Val_One, bool *Quit) // сравнивает значения
+{
+    char Val_Two;
+    printf("\n |\\\\\\| ");
+    scanf("%c", &Val_Two);
+    if(Val_Two == '!')
+    {
+        *Quit = true;
+        return true;
+    }
+    if(*Val_One == Val_Two)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Words_One()
+{
+    char Val_One;
+    int Option;
+    long int Score = -1;
+    bool Quit = false;
+    do
+    {
+        Option = rand()%2;
+        if(Option == 0)
+        {
+            Val_One = 'a' + rand()%26;
+        }
+        if(Option == 1)
+        {
+            Val_One = 'A' + rand()%26;
+        }
+        Score++;
+        do
+        {
+            system("cls");
+            printf("Для выхода введите - !\n\t\t\tСчёт: %d", Score);
+            printf("\n|||||||: %c\n", Val_One);
+        }while(Comparator(&Val_One, &Quit) != true);
+
+    }while (Quit == false);
+}
+void Words_Two()
+{
+
+}
+void Words_Three()
+{
+
+}
+void Numbers_One()
+{
+    char Val_One;
+    bool Quit = false;
+    long int Score = -1;
+    do
+    {
+        Val_One = '0' + rand()%10;
+        Score++;
+        do
+        {
+            system("cls");
+            printf("Для выхода введите - !\n\t\t\t Счёт: %d", Score);
+            printf("\n|||||||: %c\n", Val_One);
+        }while(Comparator(&Val_One, &Quit) != true);
+
+    }while (Quit == false);
+}
+void Numbers_Two()
+{
+
+}
+void Numbers_Three()
+{
+
+}
+void Words_and_Numbers()
+{
+
+}
+void Duel(int Mode, int Difficulty)
+{
+    if(Mode == 1)
+    {
+        if(Difficulty == 1)
+        {
+            Words_One();
+        }
+        else if(Difficulty == 2)
+        {
+            Words_Two();
+        }
+        else if(Difficulty == 3)
+        {
+            Words_Three();
+        }
+    }
+    else if(Mode == 2)
+    {
+        if(Difficulty == 1)
+        {
+            Numbers_One();
+        }
+        else if(Difficulty == 2)
+        {
+            Numbers_Two();
+        }
+        else if(Difficulty == 3)
+        {
+            Numbers_Three();
+        }
+    }
+    else if(Mode == 3)
+    {
+        Words_and_Numbers();
+    }
 }
 
 int main ()
 {
     setlocale(0,"Russian");
+    srand(time(NULL));
 
     int Mode = 0, Difficulty = 0;
     bool Game = true;
 
-    while(Game)
+    while(Game) // Вся логика программы
     {
-        system("cls");
-        printf("\tПривет, хочешь преисполниться в печати?\n");
-        Prepare(&Mode, &Difficulty);
-        printf("\n%d %d\n", Mode, Difficulty);
-        system("PAUSE");
-        Game = Exit(&Game);
+        system("cls");                                          //
+        printf("\tПривет, хочешь преисполниться в печати?\n");  //
+        Prepare(&Mode, &Difficulty);                            // выбираем режим игры и уровень сложности
+        printf("\n%d %d\n", Mode, Difficulty);                  // выводим значения, которые выбрал пользователь
+        Duel(Mode, Difficulty);
+
+
+
+        system("PAUSE");                                        //
+        Game = Exit(&Game);                                     // Проверяем хочет ли пользователь потренироваться ещщё раз или нет
     }
     system("cls");
     printf("Спасибо за игру, возвращяйтесь\n");
@@ -91,3 +217,4 @@ int main ()
     system("PAUSE");
     return 0;
 }
+
