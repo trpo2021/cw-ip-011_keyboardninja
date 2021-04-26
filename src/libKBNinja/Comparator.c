5086aa53ef4132d612bd500bb26a05f7cce9a3c5
T1fon <danil_ignatyev@mail.ru>
2021-04-26 22:03:16 +0700

    Add third function

---
+++ src/libKBNinja/Comparator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+
+bool Comparator(char *Val_One, bool *Quit) // Закончить игру
+{
+    char Val_Two;
+    printf("\n |\\\\\\| ");
+    Val_Two = getch();
+    if(Val_Two == '!')
+    {
+        *Quit = true;
+        return true;
+    }
+    if(*Val_One == Val_Two)
+    {
+        return true;
+    }
+    return false;
+}
