214e57468fd828d17eff633199b7ede1da944dfb
T1fon <danil_ignatyev@mail.ru>
2021-04-26 21:58:34 +0700

    Add second function

---
+++ src/libKBNinja/Exit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+
+bool Exit(bool exit) // Проверяем хочет ли пользователь закончить игру
+    system("cls");
+    char key;
+
+    printf("\Закончить игру:? Y/N\n");
+    scanf("\n%c",&key);
+
+    if (key == 'y' || key == 'Y')
+    {
+        exit = false;
+    }
+    return exit;
+}
