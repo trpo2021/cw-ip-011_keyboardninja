c053bc7d19feada3f8bce499e30a63614a72ed66
T1fon <danil_ignatyev@mail.ru>
2021-04-27 12:35:39 +0700

    Add eighth function

---
+++ src/libKBNinja/Duel.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <libKBNinja/Worlds.h>
+#include <libKBNinja/Numbers.h>
+#include <libKBNinja/Worlds_and_Numbers.h>
+
+void Duel(int Mode, int Difficulty)
+{
+    if(Mode == 1)
+    {
+        if(Difficulty == 1)
+        {
+            Words_One();
+        }
+        else if(Difficulty == 2)
+        {
+            Words_Two();
+        }
+        else if(Difficulty == 3)
+        {
+            Words_Three();
+        }
+    }
+    else if(Mode == 2)
+    {
+        if(Difficulty == 1)
+        {
+            Numbers_One();
+        }
+        else if(Difficulty == 2)
+        {
+            Numbers_Two();
+        }
+        else if(Difficulty == 3)
+        {
+            Numbers_Three();
+        }
+    }
+    else if(Mode == 3)
+    {
+        Words_and_Numbers();
+    }
+}
+
