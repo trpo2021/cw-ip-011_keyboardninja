5c587eb14a05975d72ea3343b3ef1d3e56d15eb2
T1fon <danil_ignatyev@mail.ru>
2021-04-27 12:27:50 +0700

    Add sixth function

---
+++ src/libKBNinja/Numbers.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <libKBNinja/Comparator.h>
+#include <libKBNinja/Pattern_read.h>
+
+
+void Numbers_One()
+{
+    char Val_One;
+    bool Quit = false;
+    long int Score = -1;
+    do
+    {
+        Val_One = '0' + rand()%10;
+        Score++;
+        do
+        {
+            system("cls");
+            printf("ƒл€ выхода введите - !\n\t\t\t —чЄт: %d", Score);
+            printf("\n|||||||: %c\n", Val_One);
+        }while(Comparator(&Val_One, &Quit) != true);
+
+    }while (Quit == false);
+}
+void Numbers_Two()
+{
+    FILE *Res;
+    Res = fopen("text/n2.txt","r");
+
+    Pattern_Read(Res);
+
+    fclose(Res);
+}
+void Numbers_Three()
+{
+    FILE *Res;
+    Res = fopen("text/n3.txt","r");
+
+    Pattern_Read(Res);
+
+    fclose(Res);
+}
---
+++ src/libKBNinja/Numbers.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void Numbers_One();
+void Numbers_Two();
+void Numbers_Three();
