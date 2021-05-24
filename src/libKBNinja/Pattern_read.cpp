a6c9e963b5090fb10d96ff6804d91184538420c7
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-24 17:57:30 +0700

    Мerging the Port_on_windows branch with the main

---
+++ src/libKBNinja/Pattern_read.cpp
@@ -0,0 +1,44 @@
+#include "Comparator.h"
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void Pattern_Read(FILE* Res, int testing)
+{
+    bool Quit = false;
+    int Score = 0, j = 0;
+    int n = 0;
+    char Text[60] = {}, Val_One;
+
+    while (Quit != true) {
+        rewind(Res);
+        fscanf(Res, "%d", &n);
+
+        // fgetc(Res);
+        int sz;
+        sz = rand() % n + 1;
+        for (int i = 0; i < sz; i++) {
+            fgets(Text, 60, Res);
+        }
+
+        for (int k = 0; k < (int)strlen(Text) - 1; k++) {
+            do {
+                j = k;
+                system("cls");
+                printf("Для выхода введите - !\n\t\t\tСчёт: %d", Score);
+                printf("\n|||||||: ");
+                Val_One = Text[j];
+                for (j; j < (int)strlen(Text) - 1; j++) {
+                    printf("%c", Text[j]);
+                }
+                printf("<");
+            } while (Comparator(&Val_One, &Quit, testing) != true);
+            if (Quit == true) {
+                break;
+            }
+            Score++;
+        }
+    }
+}
