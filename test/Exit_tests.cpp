72a7310b4bf0c1524f2ec209503df426ccdb9493
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-24 17:53:32 +0700

    Мerging the Port_on_windows branch with the main

---
+++ test/Exit_tests.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <string.h>
+#include <ctest.h>
+#include "../libKBNinja/Exit.h"
+
+
+// Тесты проверяют вывод булевской функции
+
+CTEST(suite2, PUSTOI)
+{
+}
+
+CTEST(suite2, Exit_test1)
+{
+	bool texit = true;
+	int testing = 1;
+	texit = Exit(testing);
+	ASSERT_TRUE(texit);
+}
+
+CTEST(suite2, Exit_test2_allwaysfalse)
+{
+	bool texit = false;
+	int testing = 1;
+	texit = Exit(testing);
+	ASSERT_FALSE(texit);
+}
