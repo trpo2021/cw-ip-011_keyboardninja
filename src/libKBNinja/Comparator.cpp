cadfdce79cf624d4f8ad214cb9005c8838376d8b
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-24 17:54:55 +0700

    Мerging the Port_on_windows branch with the main

---
+++ src/libKBNinja/Comparator.cpp
@@ -0,0 +1,24 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+
+bool Comparator(char* Val_One, bool* Quit, int testing) // СЃСЂР°РІРЅРёРІР°РµС‚ Р·РЅР°С‡РµРЅРёСЏ
+{
+    char Val_Two;
+    if (!testing) {
+        printf("\n |\\\\\\| ");
+        Val_Two = getch();
+    } else
+        Val_Two = '!';
+    if (Val_Two == '!') {
+        *Quit = true;
+        return true;
+    }
+    if (*Val_One == Val_Two) {
+        return true;
+    }
+    return false;
+}
