475bfdcd528693827ed7418f6e6dd2ca04193351
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-24 17:56:28 +0700

    Мerging the Port_on_windows branch with the main

---
+++ src/libKBNinja/Worlds_and_Numbers.cpp
@@ -0,0 +1,16 @@
+#include "Pattern_read.h"
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void Words_and_Numbers(char* Name, int testing)
+{
+    FILE* Res;
+    Res = fopen(Name, "r");
+
+    Pattern_Read(Res, testing);
+
+    fclose(Res);
+}
