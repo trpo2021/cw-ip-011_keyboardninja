fb0641b815e79b3aaf8cd294ee97ece588b68932
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-24 17:54:33 +0700

    Мerging the Port_on_windows branch with the main

---
+++ src/libKBNinja/Exit.cpp
@@ -0,0 +1,23 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+bool Exit(int testing) // Проверяем хочет ли пользователь закончить игру
+{
+    system("cls");
+    char key;
+    bool exit = true;
+    if (!testing) {
+        printf("\nЗакончить игру:? Y/N\n");
+        scanf("\n%c", &key);
+    } else {
+        key = 'y';
+    }
+
+    if (key == 'y' || key == 'Y') {
+        exit = false;
+    }
+    return exit;
+}
