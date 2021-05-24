fab014c3c6fdf5e45138cd8397c5a89643092a17
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-24 17:56:10 +0700

    Мerging the Port_on_windows branch with the main

---
+++ src/libKBNinja/Duel.cpp
@@ -0,0 +1,39 @@
+#include "Numbers.h"
+#include "Worlds.h"
+#include "Worlds_and_Numbers.h"
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void Duel(
+        int Mode,
+        int Difficulty,
+        int testing) // запуск текстов на основе выбранного уровня сложности
+{
+    if (Mode == 1) {
+        if (Difficulty == 1) {
+            Words_One(testing);
+        } else if (Difficulty == 2) {
+            char Name[] = {"text/l2.txt"};
+            Words_and_Numbers(Name, testing);
+        } else if (Difficulty == 3) {
+            char Name[] = {"text/l3.txt"};
+            Words_and_Numbers(Name, testing);
+        }
+    } else if (Mode == 2) {
+        if (Difficulty == 1) {
+            Numbers_One(testing);
+        } else if (Difficulty == 2) {
+            char Name[] = {"text/n2.txt"};
+            Words_and_Numbers(Name, testing);
+        } else if (Difficulty == 3) {
+            char Name[] = {"text/n3.txt"};
+            Words_and_Numbers(Name, testing);
+        }
+    } else if (Mode == 3) {
+        char Name[] = {"text/lan.txt"};
+        Words_and_Numbers(Name, testing);
+    }
+}
