2143556d8b7ad7fbccbb8f0a9f67231fe78c18b1
T1fon <danil_ignatyev@mail.ru>
2021-05-26 12:34:45 +0700

    Add tests for function Prepare

---
+++ test/Prepare_testscpp.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <string.h>
+#include <ctest.h>
+#include "../libKBNinja/Prepare.h"
+// Тесты будут проверять, что будет, если ввести mode и difficult > 3 или < 0
+CTEST(suite5, PUSTOI)
+{
+}
+ CTEST(suite5, Prepare1)
+{
+    int tmode = 4, tdif = 1, test =1, n;
+    n = Prepare(tmode, tdif, test);
+    ASSERT_NULL(n);
+}
+CTEST(suite5, Prepare2)
+{
+    int tmode = 1, tdif = 0, test =1, n;
+    n = Prepare(tmode, tdif, test);
+    ASSERT_NULL(n);
+}
