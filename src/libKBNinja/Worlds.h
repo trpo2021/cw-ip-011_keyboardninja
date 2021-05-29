81f23bc217c53c42631e74b2e39b3b35024b0d32
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-29 20:05:06 +0700

    Add graphics

--- src/libKBNinja/Worlds.h
+++ src/libKBNinja/Worlds.h
@@ -1,3 +1,6 @@
 #pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
-void Words_One(int testing);
+void Words_One(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, int *Screen, int testing);
