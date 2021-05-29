dabb0f5749ef8944b70c13d9f78e454921d96e9d
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-29 20:07:26 +0700

    Added a custom graphics library

---
+++ src/libKBNinja/Trpo_SDL.h
@@ -0,0 +1,13 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#pragma once
+
+void Load (SDL_Window **MainWindow,  SDL_Renderer **MainRender, SDL_Surface **MainSurface, TTF_Font **MainFont);
+void Close (SDL_Window **MainWindow, SDL_Renderer **MainRender, SDL_Texture **MainTexture, SDL_Surface **MainSurface, TTF_Font **MainFont);
+SDL_Rect Boxing(int x, int y, int Wight, int Hight);
+void Copy_Render(SDL_Texture **MainTexture,SDL_Surface **Background, SDL_Renderer **MainRender, SDL_Rect *Box);
+void Print_Text(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, char *Text, SDL_Rect *Box);
+void Moving_Coursor(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, SDL_Event *Event, int Screen, int *Position, int Mode);
+void LoadScreen (SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont,int Screen);
+bool Analysis_Event(SDL_Event Event);
