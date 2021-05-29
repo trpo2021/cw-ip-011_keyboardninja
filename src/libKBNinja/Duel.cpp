ba32eac3ed95047c7ffec1fd9b93939cdb4b4be1
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-29 20:04:31 +0700

    Add graphics

--- src/libKBNinja/Duel.cpp
+++ src/libKBNinja/Duel.cpp
@@ -8,32 +8,35 @@
 #include <time.h>
 
 void Duel(
-        int Mode,
-        int Difficulty,
-        int testing) // запуск текстов на основе выбранного уровня сложности
-{
-    if (Mode == 1) {
-        if (Difficulty == 1) {
-            Words_One(testing);
-        } else if (Difficulty == 2) {
+    SDL_Renderer **MainRender,
+    SDL_Texture **MainTexture,
+    TTF_Font **MainFont,
+    int *Mode,
+    int *Difficulty,
+    int *Screen,
+    int testing) { // запуск текстов на основе выбранного уровня сложности
+    if (*Mode == 1) {
+        if (*Difficulty == 1) {
+            Words_One(MainRender,MainTexture,MainFont,Screen, testing);
+        } else if (*Difficulty == 2) {
             char Name[] = {"text/l2.txt"};
-            Words_and_Numbers(Name, testing);
-        } else if (Difficulty == 3) {
+            Words_and_Numbers(MainRender,MainTexture,MainFont,Name, Screen, testing);
+        } else if (*Difficulty == 3) {
             char Name[] = {"text/l3.txt"};
-            Words_and_Numbers(Name, testing);
+            Words_and_Numbers(MainRender,MainTexture,MainFont,Name, Screen, testing);
         }
-    } else if (Mode == 2) {
-        if (Difficulty == 1) {
-            Numbers_One(testing);
-        } else if (Difficulty == 2) {
+    } else if (*Mode == 2) {
+        if (*Difficulty == 1) {
+            Numbers_One(MainRender,MainTexture,MainFont,Screen, testing);
+        } else if (*Difficulty == 2) {
             char Name[] = {"text/n2.txt"};
-            Words_and_Numbers(Name, testing);
-        } else if (Difficulty == 3) {
+            Words_and_Numbers(MainRender,MainTexture,MainFont,Name, Screen, testing);
+        } else if (*Difficulty == 3) {
             char Name[] = {"text/n3.txt"};
-            Words_and_Numbers(Name, testing);
+            Words_and_Numbers(MainRender,MainTexture,MainFont,Name, Screen, testing);
         }
-    } else if (Mode == 3) {
+    } else if (*Mode == 3) {
         char Name[] = {"text/lan.txt"};
-        Words_and_Numbers(Name, testing);
+        Words_and_Numbers(MainRender,MainTexture,MainFont,Name, Screen, testing);
     }
 }
