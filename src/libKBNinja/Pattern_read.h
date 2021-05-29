b84f6968b260cd89bcf3aef89a12d3c1d6b8345e
Ivan Shuljenko <ilitetop@yandex.ru>
2021-05-29 20:05:58 +0700

    Add graphics

--- src/libKBNinja/Pattern_read.h
+++ src/libKBNinja/Pattern_read.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 #pragma once
 
-void Pattern_Read(FILE* Res, int testing);
+void Pattern_Read(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, int *Screen, FILE* Res, int testing);
