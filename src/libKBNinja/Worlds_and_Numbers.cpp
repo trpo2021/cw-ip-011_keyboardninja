f1c2a9e4126f3f597af876efe76b5cb747c60a8a
T1fon <danil_ignatyev@mail.ru>
2021-05-30 20:28:32 +0700

    Change code for testing

--- src/libKBNinja/Worlds_and_Numbers.cpp
+++ src/libKBNinja/Worlds_and_Numbers.cpp
@@ -6,11 +6,19 @@
 #include <time.h>
 
 
-void Words_and_Numbers(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, char* Name, int *Screen, int testing) {
+bool Words_and_Numbers(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, char* Name, int *Screen, int testing) {
     FILE* Res;
     Res = fopen(Name, "r");
+    if (!testing){
+        Pattern_Read(MainRender, MainTexture, MainFont, Screen, Res, testing);
+    }
+    else if (Res != NULL){
+        return 1;
+        }
+    else{
+        return 0;
+        }
 
-    Pattern_Read(MainRender, MainTexture, MainFont, Screen, Res, testing);
 
     fclose(Res);
 }
