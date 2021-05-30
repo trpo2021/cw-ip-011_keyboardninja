98c9e5af963dd2f8a93477bb8a5b55f0aed2826d
T1fon <danil_ignatyev@mail.ru>
2021-05-30 20:23:11 +0700

    Change tipe of function from void to int

--- src/libKBNinja/Duel.h
+++ src/libKBNinja/Duel.h
@@ -4,6 +4,6 @@
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 
-void Duel(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, int *Mode, int *Difficulty, int *Screen, int testing);
+int Duel(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, int *Mode, int *Difficulty, int *Screen, int testing);
 
 
