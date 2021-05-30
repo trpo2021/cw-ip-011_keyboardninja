99871d087d2f2c617b58814d00742f0e93127960
T1fon <danil_ignatyev@mail.ru>
2021-05-30 20:29:15 +0700

    Modified tests

--- test/Words_and_Numbers_tests.cpp
+++ test/Words_and_Numbers_tests.cpp
@@ -1,8 +1,14 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
 #include <locale.h>
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <math.h>
+#define SCREEN_WIGHT 1280
+#define SCREEN_HIGH 720
 #include <ctest.h>
 #include "../libKBNinja/Words_and_Numbers.h"
 // Тесты будут проверять, как открывается файл
@@ -11,15 +17,21 @@ CTEST(suite4, PUSTOI)
 }
  CTEST(suite4, Words_and_Numbers1)
 {
-   FILE* real;
-   int test = 1;
-    real=Words_and_Numbers("test/test.txt", test);
-    ASSERT_NOT_NULL(real);
+    SDL_Renderer **MainRender;
+    SDL_Texture **MainTexture;
+    TTF_Font **MainFont;
+   char Name[] = {"test/test.txt"};
+   int test = 1, screen = 1, real = 0, k = 1;
+    real=Words_and_Numbers(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, &Name, &screen, test);
+    ASSERT_EQUAL(k, real);
 }
  CTEST(suite4, Words_and_Numbers2)
 {
-   FILE* real;
-   int test = 1;
-    real=Words_and_Numbers("rb.txt", test);
-    ASSERT_NULL(real);
+    SDL_Renderer **MainRender;
+    SDL_Texture **MainTexture;
+    TTF_Font **MainFont;
+   char Name[] = {"test/qw.txt"};
+   int test = 1, screen = 1, real = 0, k = 1;
+    real=Words_and_Numbers(SDL_Renderer **MainRender, SDL_Texture **MainTexture, TTF_Font **MainFont, &Name, &screen, test);
+    ASSERT_NOT_EQUAL(k, real);
 }
