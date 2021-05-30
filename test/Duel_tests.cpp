442364e4d6c06fc18c6a64ff4587e62a84988e7f
T1fon <danil_ignatyev@mail.ru>
2021-05-30 20:30:00 +0700

    Modified tests

--- test/Duel_tests.cpp
+++ test/Duel_tests.cpp
@@ -1,8 +1,11 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include <time.h>
 #include <string.h>
+#include <time.h>
+#include <math.h>
+#define SCREEN_WIGHT 1280
+#define SCREEN_HIGH 720
 #include <ctest.h>
 #include "../libKBNinja/Duel.h"
 
@@ -11,29 +14,17 @@
 CTEST(suite3, PUSTOI)
 {
 }
+//Следующие два теста проверяют, что будет, если ввести mode и difficulty вне их значений. Делаю через equal, так как не работает null. ПОэттому ввел константу k =1
 CTEST(suite3, Duel_test1)
 {
-	int tmode = 4, tdif = 1, test = 0, n;
-	n = Duel(tmode, tdif, test);
-	ASSERT_NULL(n);
+	int tmode = 4, tdif = 1, test = 0, n, k = 1, screen = 0;
+	n = Duel(&tmode, &tdif, &screen, test);
+	ASSERT_NOT_EQUAL(k, n);
 }
 CTEST(suite3, Duel_test2)
 {
-	int tmode = 1, tdif = 4, test = 0, n;
-	n = Duel(tmode, tdif, test);
-	ASSERT_NULL(n);
-}
-// Проверим, что будет, если открыть файл
-CTEST(suite3, Duel_test3)
-{
-	int tmode = 1, tdif = 2, test = 1, n, k = 1;
-	n = Duel(tmode, tdif, test);
-	ASSERT_EQUAL(k, n);
-}
- // Проверим, что будет, если открыть несуществующий  файл
-CTEST(suite3, Duel_test4)
-{
-	int tmode = 1, tdif = 3, test = 1, n, k = 1;
-	n = Duel(tmode, tdif, test);
+	int tmode = 1, tdif = 4, test = 0, n, k =1, screen = 0;
+	n = Duel(&tmode, &tdif, &screen, test);
 	ASSERT_NOT_EQUAL(k, n);
 }
+
